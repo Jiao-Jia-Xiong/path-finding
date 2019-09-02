98572d60ea1d3791bfb62600b1d73d65507de9b9
Jiao-Jia-Xiong <joejiao8234@gmail.com>
2019-09-02 02:06:12 -0400

    added unit test for map.py

---
+++ test_map.py
@@ -0,0 +1,13 @@
+"""Unit test for map module"""
+import unittest
+from map import Map
+
+
+class TestMap(unittest.TestCase):
+
+    def test_init(self):
+        self.map = Map((100, 100))
+        self.assertEqual(100, len(self.map.nodes))
+        self.assertEqual(100, len(self.map.nodes[0]))
+        self.assertEqual((9, 9), self.map[9, 9].loc)
+        self.assertEqual((0, 0), self.map[0, 0].loc)
